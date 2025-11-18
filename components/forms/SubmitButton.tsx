fbc56668a17a65a9571012ff3ed76c417d658514
ChMubasharAli <muhammadmubashirali075@gmail.com>
2025-11-18 21:05:07 +0500

    change the styling of the submit button

--- components/forms/SubmitButton.tsx
+++ components/forms/SubmitButton.tsx
@@ -14,7 +14,7 @@ export const SubmitButton = ({
   return (
     <button
       disabled={disabled}
-      className="px-6 py-3 font-semibold bg-blue-600 text-white rounded-[8px] hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors duration-200  disabled:cursor-not-allowed"
+      className="px-6 py-3 font-semibold bg-blue-600  text-white rounded-[8px] hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-colors duration-200  disabled:cursor-not-allowed  border-2 border-blue-600 "
     >
       {loading ? "SUBMITTING..." : label}
     </button>
